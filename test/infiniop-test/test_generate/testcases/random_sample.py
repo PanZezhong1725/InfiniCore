36546877926c5e7d2a3a4239b97e09b6c92a5f39
xgqdut2016 <kenan_gewei@163.com>
2025-03-17 14:58:19 +0800

    issue/108: add result tensor and modified random_sample.py

--- test/infiniop-test/test_generate/testcases/random_sample.py
+++ test/infiniop-test/test_generate/testcases/random_sample.py
@@ -6,10 +6,22 @@ from typing import List
 from .. import InfiniopTestWriter, InfiniopTestCase, np_dtype_to_ggml, gguf_strides
 
 
+def sample(voc, topk, dtype):
+    assert voc >= topk
+    x = np.random.rand(topk) + 1
+    y = np.random.rand(voc - topk)
+
+    # 将x和y合并，并打乱顺序
+    z = np.concatenate([x, y]).astype(dtype)
+    np.random.shuffle(z)
+
+    return z
+
+
 def softmax(x, axis):
     M = np.max(x, axis=axis, keepdims=True)
     tmp = np.exp(x - M)
-    return tmp / np.sum(tmp, axis = axis, keepdims=True)
+    return tmp / np.sum(tmp, axis=axis, keepdims=True)
 
 
 def random_sample(
@@ -20,7 +32,7 @@ def random_sample(
     voc: int = 32000,
     temperature: float = 1.0,
 ):
-    result = np.zeros([1])
+    result = np.zeros([1]).astype(np.int64)
     if topp > 0 and topk > 1:
         indices = np.zeros([topk], dtype=np.int64)
         dataNp = data.copy()
@@ -40,11 +52,9 @@ def random_sample(
         # sorted_indices = np.argsort(dataNp, descending=True)
         indices = sorted_indices[:topk]
 
-        dataNp = dataNp[sorted_indices]
-
         globalM = dataNp[0]
         dataNp = (dataNp - globalM) / temperature
-        dataNp = softmax(dataNp, axis = 0)
+        dataNp = softmax(dataNp, axis=0)
         sum_s = 0
         for end in range(topk):
             sum_s += dataNp[end]
@@ -65,7 +75,9 @@ def random_sample(
             sum_s += dataNp[i]
             if random_val < sum_s:
                 result[0] = indices[i]
-                return result
+                break
+
+        return result
     else:
         result[0] = np.argmax(data)
         return result
@@ -89,7 +101,7 @@ class RandomSampleTestCase(InfiniopTestCase):
         self.temperature = temperature
 
     def write_test(self, test_writer: "InfiniopTestWriter"):
-        super().write_test(test_writer) 
+        super().write_test(test_writer)
         test_writer.add_float32(test_writer.gguf_key("random_val"), self.random_val)
         test_writer.add_float32(test_writer.gguf_key("topp"), self.topp)
         test_writer.add_int32(test_writer.gguf_key("topk"), self.topk)
@@ -103,12 +115,14 @@ class RandomSampleTestCase(InfiniopTestCase):
             self.voc,
             self.temperature,
         )
+        result = np.zeros([1]).astype(np.int64)
         test_writer.add_tensor(
-            test_writer.gguf_key("data"), self.data, raw_dtype=np_dtype_to_ggml(self.data.dtype)
+            test_writer.gguf_key("data"),
+            self.data,
+            raw_dtype=np_dtype_to_ggml(self.data.dtype),
         )
-        test_writer.add_tensor(
-            test_writer.gguf_key("ans"), ans, raw_dtype=gguf.GGMLQuantizationType.F64
-        )
+        test_writer.add_tensor(test_writer.gguf_key("ans"), ans)
+        test_writer.add_tensor(test_writer.gguf_key("result"), result)
 
 
 if __name__ == "__main__":
@@ -116,112 +130,112 @@ if __name__ == "__main__":
     # data, random_val, topp, topk, temperature
     test_cases = [
         RandomSampleTestCase(
-            np.random.rand(512).astype(np.float32),
+            sample(512, 3, np.float32),
             0.8,
             0.8,
             3,
             0.5,
         ),
         # RandomSampleTestCase(
-        #     np.random.rand(4096).astype(np.float32),
+        #     sample(4096, 5, np.float32),
         #     0.05,
         #     0.9,
         #     5,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(16384).astype(np.float32),
+        #     sample(16384, 10, np.float32),
         #     0.15,
         #     0.85,
         #     10,
         #     2.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(512).astype(np.float32),
+        #     sample(512, 3, np.float32),
         #     0.08,
         #     0,
         #     3,
         #     0.5,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(4096).astype(np.float32),
+        #     sample(4096, 1, np.float32),
         #     0.5,
         #     0.9,
         #     1,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(16384).astype(np.float32),
+        #     sample(16384, 1, np.float32),
         #     0.15,
         #     0,
         #     1,
         #     2.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(32000).astype(np.float32),
+        #     sample(32000, 50, np.float32),
         #     0.08,
         #     0.8,
         #     50,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(32000).astype(np.float32),
+        #     sample(32000, 25, np.float32),
         #     0.08,
         #     1.0,
         #     25,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(512).astype(np.float16),
+        #     sample(512, 3, np.float16),
         #     0.8,
         #     0.8,
         #     3,
         #     0.5,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(4096).astype(np.float16),
+        #     sample(4096, 5, np.float16),
         #     0.05,
         #     0.9,
         #     5,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(16384).astype(np.float16),
+        #     sample(16384, 10, np.float16),
         #     0.15,
         #     0.85,
         #     10,
         #     2.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(512).astype(np.float16),
+        #     sample(512, 3, np.float16),
         #     0.08,
         #     0,
         #     3,
         #     0.5,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(4096).astype(np.float16),
+        #     sample(4096, 1, np.float16),
         #     0.5,
         #     0.9,
         #     1,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(16384).astype(np.float16),
+        #     sample(16384, 1, np.float16),
         #     0.15,
         #     0,
         #     1,
         #     2.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(32000).astype(np.float16),
+        #     sample(32000, 50, np.float16),
         #     0.08,
         #     0.8,
         #     50,
         #     1.0,
         # ),
         # RandomSampleTestCase(
-        #     np.random.rand(32000).astype(np.float16),
+        #     sample(32000, 25, np.float16),
         #     0.08,
         #     1.0,
         #     25,
@@ -230,4 +244,3 @@ if __name__ == "__main__":
     ]
     test_writer.add_tests(test_cases)
     test_writer.save()
-
